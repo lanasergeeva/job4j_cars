8dac506a6cc9023c8e4e749c85473b36848eae5c
lanasergeeva <marikakopina@gmail.com>
2021-11-19 14:38:42 +0300

    поправила скрипт

--- db/scripts/cars.sql
+++ db/scripts/cars.sql
@@ -23,8 +23,8 @@ CREATE TABLE public.ads
 (
     id          serial primary key,
     created     timestamp          NOT NUL,
-    description character varying(255),
-    name        character varying(255),
+    description character varying(255) NOT NUL,
+    name        character varying(255) NOT NUL,
     status      boolean            NOT NULL,
     body_id references bodies (id) NOT NULL,
     mark_id references marks (id)  NOT NULL,
